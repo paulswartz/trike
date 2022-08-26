f89d911b1a0bf1485ec25282579fff398b569e47
Paul Swartz <paul@paulswartz.net>
2022-08-26 19:37:28 -0400

    fix: use the fake KinesisClient if sending to the console

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,5 +1,13 @@
 import Config
 
+kinesis_stream = System.get_env("KINESIS_STREAM", "console")
+
 config :trike,
   listen_port: System.get_env("LISTEN_PORT", "8001") |> String.to_integer(),
-  kinesis_stream: System.get_env("KINESIS_STREAM", "console")
+  kinesis_stream: kinesis_stream
+
+if kinesis_stream == "console" do
+  # use fake client if logging to the console
+  config :trike,
+    kinesis_client: Fakes.FakeKinesisClient
+end
